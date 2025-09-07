6821f7cb91f5f355f9b940b10822c38b840e8252
Von Mendres <mendresvon@gmail.com>
2025-09-07 21:32:38 +0800

    added my chinese name to the navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -38,7 +38,7 @@ export default function Navbar() {
       <div className="container mx-auto px-6 py-4 flex justify-between items-center">
         {/* Your Name/Logo */}
         <div className="font-bold text-lg">
-          <Link href="#hero">Von Mendres</Link>
+          <Link href="#hero">Von Mendres「馬盛中」</Link>
         </div>
 
         {/* Desktop Navigation Links */}
