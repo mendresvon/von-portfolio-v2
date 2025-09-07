184fe9482f18227b35b538b9041ca380702e25fc
Von Mendres <mendresvon@gmail.com>
2025-09-07 23:55:04 +0800

    removed project cover image animation on hover

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -39,7 +39,7 @@ export default function ProjectCard({ project }: ProjectCardProps) {
                 src={project.imageUrl}
                 alt={project.title}
                 fill
-                className="object-cover transition-transform duration-300 group-hover:scale-105"
+                className="object-cover transition-transform duration-300"
               />
             </div>
           </Link>
